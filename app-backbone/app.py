6c015b30e21fbe952b28557dd292aa0510505b8d
Oskar Mikus <oskarmikus@protonmail.com>
2021-06-11 19:56:52 +0200

    Heroku test

--- app-backbone/app.py
+++ app-backbone/app.py
@@ -1,6 +1,10 @@
+import os
+
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
@@ -9,4 +13,5 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
